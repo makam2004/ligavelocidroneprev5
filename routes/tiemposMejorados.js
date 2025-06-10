a03cbe34d90e226ed3cc7d9af1f4fc11c863ec1e
MIGUEL ANGEL CHILLON VAZQUEZ <zc01331@tc2.tech>
2025-06-10 14:03:03 +0200

    Cambio1

--- routes/tiemposMejorados.js
+++ routes/tiemposMejorados.js
@@ -1,6 +1,6 @@
 // routes/tiemposMejorados.js
 import express from 'express';
-import supabase from '../supabaseClient'; // Si usas Supabase
+import supabase from '../supabaseClient';
 
 const router = express.Router();
 
